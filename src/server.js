f86aa3ee574e44d18ff7e34fc21f8f2ce3e338d3
Meriemchm <chamimeriem2302@gmail.com>
2025-10-10 11:33:03 +0100

    add routes api

---
+++ src/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import subCategoryRoutes from "./routes/subCategoryRoutes.js";
+import likeRoutes from "./routes/likeRoutes.js";
+import readingStatusRoutes from "./routes/readingStatusRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+app.use("/api/users", userRoutes);
+
+app.use("/api/books", bookRoutes);
+
+app.use("/api/categories", categoryRoutes);
+
+app.use("/api/subcategories", subCategoryRoutes);
+
+app.use("/api/readingstatus",readingStatusRoutes);
+
+app.use("api/likes", likeRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running perfectly!");
+});
+// Démarrer le serveur
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
