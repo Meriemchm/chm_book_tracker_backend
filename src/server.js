996fb214fd2a6469d5014e590f2d057a36fcf9bc
Meriemchm <116971809+Meriemchm@users.noreply.github.com>
2025-11-14 17:01:33 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-// Autoriser toutes les origines (pour dev)
+app.use(express.json());
 app.use(cors({
   origin: "http://localhost:3000", // frontend
   credentials: true,              
